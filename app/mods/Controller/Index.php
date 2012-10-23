3dc6419ee1ba425ddc6c8d18273b4e5abd2f118f
Paul Soberanes <paulsoberanes@gmail.com>
2012-10-23 17:24:23 -0500

    TODO

--- app/mods/Controller/Index.php
+++ app/mods/Controller/Index.php
@@ -4,11 +4,9 @@ class Controller_Index extends Controller_Abstract{
 	protected $_controller = 'index';
 	
 	public function indexAction(){
-		header("Location:index/home");
-	}
-	
-	public function homeAction(){
 		
 	}
+	
+	
 
 }
