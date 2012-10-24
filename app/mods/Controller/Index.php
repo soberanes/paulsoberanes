1beabeeff600faf52d5f7dea7744258580b0627b
Paul Soberanes <paulsoberanes@gmail.com>
2012-10-23 19:14:12 -0500

    contacto

--- app/mods/Controller/Index.php
+++ app/mods/Controller/Index.php
@@ -7,6 +7,28 @@ class Controller_Index extends Controller_Abstract{
 		
 	}
 	
-	
+	public function contactoAction(){
+		
+	}
+
+	public function recibeAction(){
+		
+		if($_POST['name'] != '' && $_POST['email'] != '' && $_POST['message'] != '' && $_POST['human'] == 4){
+			
+			$nombre = $_POST['name'];
+			$email  = $_POST['email'];
+			$message = $_POST['message'];
+
+			$to      = 'paul@paulsoberanes.com';
+			$titulo = 'Nuevo Mensaje de Contacto';
+			
+			$cabeceras = "From:".$nombre." (".$email.")" . "\r\n" .
+			    'Reply-To: paul@paulsoberanes.com' . "\r\n" .
+			    'X-Mailer: PHP/' . phpversion();
+
+			mail($para, $titulo, $mensaje, $cabeceras);
+
+		}
+	}
 
 }
